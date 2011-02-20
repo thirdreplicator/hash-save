8bc90105c861a855d00f1a779087e1b01c54d378
David Beckwith <thirdreplicator@gmail.com>
2011-02-20 05:57:36 -0800

    Added some rspec tests.  Added Hash#load functionality.

--- lib/hash-save.rb
+++ lib/hash-save.rb
@@ -7,26 +7,43 @@ class Hash
         Marshal.dump(v, f)
       end
     end
+
     rescue Errno::ENOENT
       Dir.mkdir(BASE_DIR)
-      retry if Dir.exists(BASE_DIR)
+      retry if Dir.exists?(BASE_DIR)
   end
 
-  def self.find(k)
+  def load!
+    self.each do |k,v|
+      self[k] = self.class.find!(k)
+    end
+  end
+
+  def self.find!(k)
     File.open("#{BASE_DIR}/#{k}", 'rb') do |f|
       Marshal.load(f)
     end
   end
 
-  def self.modify(k)
+  def load
+    self.each do |k,v|
+      self[k] = self.class.find(k)
+    end
+  end
+
+  def self.find(k, default_val=nil)
+    begin
+      self.find!(k)
+    rescue Errno::ENOENT => e
+      default_val
+    end
+  end
+
+  def self.modify!(k)
     {k => yield(Hash.find(k))}.save
   end
 
-  def self.inc(k)
-    modify(k){|x| x + 1}
-  end
-
   def self.inc!(k)
-    modify(k){|x| x + 1}.save
+    modify!(k){ |x| x + 1 }
   end
 end
---
+++ test/test_hash-save.rb
@@ -0,0 +1,67 @@
+$LOAD_PATH << File.join(File.dirname( File.expand_path(__FILE__)), '..', 'lib')
+require 'hash-save'
+require 'rspec'
+
+describe Hash do 
+
+  before :each do
+    `rm -rf ./data`
+  end
+
+  it "should be able to save a key's values." do
+    {:a => 1}.save
+    Hash.find(:a).should == 1
+  end
+
+  it "should be able to save a key's values (strings)." do
+    {:a => "pizza"}.save
+    Hash.find(:a).should == "pizza"
+  end
+
+  it "should be able to save a key's values (arrays)." do
+    {:a => ["pizza", 42]}.save
+    Hash.find(:a).should == ["pizza", 42]
+  end
+
+  it "should be able to save a key's values (hashes)." do
+    {:a => {"pizza" => 42}}.save
+    Hash.find(:a).should == {"pizza" => 42}
+  end
+
+  it "should create a data directory if one doesn't exist" do
+    Dir.exists?('data').should be_false
+    {:a => 1}.save
+    Dir.exists?('data').should be_true
+  end
+
+  it "should be able to take single-argument function to create a new modified hash instance based on its saved value." do
+    {:a => 42}.save
+    Hash.modify!(:a){ |x| 10 + x }
+    Hash.find(:a).should == 52
+    # The version on disk should have changed.
+    Hash.find(:a).should == 52
+  end
+
+  it "should be able to increment integer values" do
+    {:a => 42}.save
+    Hash.inc!(:a).should == {:a => 43}
+    # The version on disk should have changed.
+    Hash.find(:a).should == 43
+  end
+
+  it "should be able to reload key values" do
+    {:a => 1, :b => 2}.save
+    h = {:a => nil, :b => nil}.load!
+    h[:a].should == 1
+    h[:b].should == 2
+  end
+
+  it "should be able to reload key values" do
+    {:a => 1, :b => 2}.save
+    h = {:a => nil, :b => nil, :c => "burgers"}.load
+    h[:a].should == 1
+    h[:b].should == 2
+    h[:c].should == nil
+  end
+
+end
